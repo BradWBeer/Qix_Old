927972780d67f55e9cda1db4912123bc7bdf5de5
Brad Beer <BradWBeer@gmail.com>
2010-08-02 21:38:59 -0400

    first Commit

---
+++ mover.lisp
@@ -0,0 +1,66 @@
+(in-package #:qix)
+
+(defclass mover ()
+  ((period
+    :accessor period
+    :initform 1
+    :initarg :period)
+   (time-offset
+    :accessor time-offset
+    :initform 0
+    :initarg :time-offset)
+   (start-value
+    :accessor start-value
+    :initform 0
+    :initarg :start-value)
+   (end-value
+    :accessor end-value
+    :initform 1
+    :initarg :end-value)
+   (repeat
+    :accessor repeat
+    :initform nil
+    :initarg :repeat)
+   (functor
+    :accessor functor
+    :initform nil
+    :initarg :functor)
+   (start-time
+    :accessor start-time
+    :initform nil)
+   (slope)
+   (last-value
+    :initform nil)))
+
+(defmethod initialize-instance :after ((this mover) &key ) 
+	   (with-slots (slope end-value start-value period) this
+	     (setf slope (/ (- end-value start-value) period))))
+	     ;(if start-now (start-mover this delay))))
+
+  
+(defmethod start-mover ((this mover) &key delay)
+  (let ((now (/ (get-internal-real-time) internal-time-units-per-second)))
+    (if delay
+	(setf (start-time this) (+ now delay)) 
+	      (setf (start-time this) now))))
+	       
+
+(defmethod get-current-value ((this mover))
+  (let ((now (/ (get-internal-real-time) internal-time-units-per-second)) (ret))
+    (setf ret 
+	  (if (> (start-time this) now)
+	      (start-value this)
+	      (if (and (not (repeat this))
+		       (< (+ (start-time this) (period this))
+			  now))
+		  (end-value this)     
+		  (rem (* (slot-value this 'slope) 
+			  (- now (start-time this)))
+		       (end-value this)))))
+    (let ((retval (if (functor this)
+		      (funcall (functor this) ret)
+		      ret))
+	  (old-value (slot-value this 'last-value)))
+      (values (setf (slot-value this 'last-value) retval)
+	      (not (equal old-value retval))))))
+
---
+++ texture.lisp
@@ -0,0 +1,50 @@
+(in-package #:qix)
+
+;; USe PBO's Pixel Buffer Objects...
+(defclass gl-texture ()
+  ((id)
+   (tex-type
+    :reader tex-type
+    :initform :texture-2d
+    :initarg :tex-type)))
+    
+(defmethod get-id ((this gl-texture))
+  (slot-value this 'id))
+
+(defmethod bind ((this gl-texture))
+;  (gl:enable :blend :texture-2d :depth-test)
+;  (gl:blend-func :src-alpha :one-minus-src-alpha)
+;  (gl:color-material :front :ambient-and-diffuse)
+  (gl:bind-texture (tex-type this) (get-id this)))
+
+(defmethod release-texture ((this gl-texture))
+  (if (get-id this)
+      (progn 
+	(gl:delete-textures (list (get-id this)))
+	(setf (slot-value this 'id) nil))))
+
+(defmethod initialize-instance :after ((this gl-texture) &key data)				     
+  (unless (slot-boundp this 'id)
+    (setf (slot-value this 'id) (car (gl:gen-textures 1)))))
+
+(defmethod load-2d-image ((this gl-texture) 
+			  data 
+			  width 
+			  height &key
+			  (format :bgra)
+			  (data-type :unsigned-byte)
+			  (wrap-s :repeat) 
+			  (wrap-t :repeat)
+			  (mag-filter :nearest)
+			  (min-filter :nearest)
+			  (perspective-correction-hint :nicest)
+			  (stride))
+  (if stride (gl:pixel-store :unpack-row-length stride))
+  (bind this)
+  (gl:tex-parameter :texture-2d :texture-wrap-s wrap-s)
+  (gl:tex-parameter :texture-2d :texture-wrap-t wrap-t)
+  (gl:tex-parameter :texture-2d :texture-mag-filter mag-filter)
+  (gl:tex-parameter :texture-2d :texture-min-filter min-filter)
+  (gl:hint :perspective-correction-hint perspective-correction-hint)
+  
+  (gl:tex-image-2d :texture-2d 0 :rgba width height 0 format data-type data))
