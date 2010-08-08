c9150263d8d40fda92d10229082a91f2957cc285
Brad Beer <BradWBeer@gmail.com>
2010-08-08 12:24:41 -0400

    getting ready for repl

--- data.lisp
+++ data.lisp
@@ -1,5 +1,20 @@
 ;(in-package #:qix)
 
+(defmacro string-concat (&rest args)
+  `(concatenate 'string ,@args))
+	 
+
+(defun split-on-newline (string)
+	   "Returns a list of substrings of string
+divided by ONE newline each.
+Note: Two consecutive spaces will be seen as
+if there weak-pointer-value an empty string between them."
+	   (loop for i = 0 then (1+ j)
+	      as j = (position #\newline string :start i)
+	      collect (subseq string i j)
+	      while j))
+
+
 (defun string-make (count)
   (make-array count :element-type 'character :adjustable t))
 
@@ -14,17 +29,42 @@
 
 (defun string-insert (string text &optional (pos (1- (length string))))
   (concatenate 'string
-			     (subseq string 0 pos)
-			     text
-			     (subseq string pos (1- (length string)))))
-	       
+	       (subseq string 0 pos)
+	       text
+	       (subseq string pos (length string))))
 
-(defun split-on-newline (string &optional (pos 0))
-	   (let ((foundp (position #\newline test-test :start pos)))
-	     (if foundp
-		 (list* (subseq string pos foundp)
-			 (split-on-newline string (1+ foundp)))
-		 string)))
+
+	       
+(defun string-list-insert (data str cursor)
+  (let* ((newlist (split-on-newline str))
+	 (length-newlist (length newlist)))
+    (cond ((= length-newlist 1)
+	   (setf (nth (first cursor) data) (string-insert (nth (first cursor) data) str (second cursor))))
+	   
+	  ((= length-newlist 2)
+	   (let ((broken-string (break-string (nth (first cursor) data) (second cursor))))
+	     (setf (nth (first cursor) data)
+		   (string-concat (first broken-string)
+				  (first newlist)))
+	     (list-insert-item data
+			       (string-concat (second newlist)
+					      (second broken-string))
+			       (1+ (first cursor)))))
+	  
+	  ((> length-newlist 2)
+	   (let ((broken-string (break-string (nth (first cursor) data) (second cursor))))
+	     (setf (nth (first cursor) data)
+		   (string-concat (first broken-string)
+				  (first newlist)))
+	     (list-insert-list data (cdr newlist) (1+ (first cursor)))
+	     (setf (nth (+ (first cursor)
+			   (1- (length newlist))) data)
+		   (string-concat (nth (+ (first cursor)
+					  (1- (length newlist))) data)
+				  (second broken-string))))))
+    data))
+	  
+   
 
 (defun string-remove (string pos &optional (len 1))
   (let ((str-len (length string)))
@@ -34,8 +74,30 @@
 	  (t (concatenate 'string 
 			  (subseq string 0 pos)
 			  (subseq string (+ pos len)))))))
-		    
 
+(defun break-string (string pos)
+	   (list (subseq string 0 pos)
+		 (subseq string pos)))		    
+
+
+(defun get-substring-list (data start &optional (end))
+  (let ((start-line (first start))
+	(start-index (second start)))
+    (if end
+	(let ((end-line (first end))
+	      (end-index (second end)))
+	  ;; simplest case, on the same line...
+	  (cond ((= start-line end-line) (subseq (nth start-line data) start-index end-index))
+		((= 1 (- end-line start-line))
+		 (format nil "~A~%~A"
+			 (subseq (nth start-line data) start-index)
+			 (subseq (nth end-line data) 0 end-index)))
+		(t (format nil "~A~%~A~A"
+			   (subseq (nth start-line data) start-index)
+			   (format nil "~{~A~%~}" (loop for i from (1+ start-line) to (1- end-line)
+						     collect (nth i data))) 
+			   (subseq (nth end-line data) 0 end-index))))))))	   	   
+				  					  
 
 (defun list-insert-item (l new-item &optional (pos 0))
   (if (>= pos (length l))
--- repl.lisp
+++ repl.lisp
@@ -1,4 +1,12 @@
-
+(defun read-whole-string-or-nothing (str)
+  (let ((chars-read 0)
+	(strlen (length str)))
+    (handler-case 
+	(loop while (< chars-read strlen)
+	   collect (multiple-value-bind (exp len) (read-from-string (subseq str chars-read))
+		     (incf chars-read len)
+		     exp))
+      (end-of-file () nil))))
 
 
 (defmacro with-retry (() &rest body)
@@ -16,49 +24,71 @@
        (,f-name))))
 
 
-(with-retry () 
-  (let ((*standard-output* (make-string-output-stream))
-	(*error-output* (make-string-output-stream)))
-    (list (eval (read))
-	  (get-output-stream-string *standard-output*)
-	  (get-output-stream-string *debug-io*))))
-
-
-(with-retry ()
-  (loop 
-     with last-call 
-     for cmd in  
-       (with-input-from-string (s "(print \"hello\") nil")
-	 (loop 
-	    with end-of-file = (gensym)
-	    for i = (read s nil end-of-file)
-	    while (not (eql i end-of-file))
-	    collect i))
-     do (setf last-call (eval cmd))
-     finally (return last-call)))
-
-
-
 (defun eval-string (str)
-  (with-retry ()
-    (let ((*standard-output* (make-string-output-stream))
-	  (*error-output* (make-string-output-stream)))
-      (list 
-       (loop 
-	  with last-call 
-	  for cmd in  
-	    (with-input-from-string (s str)
-	      (loop 
-		 with end-of-file = (gensym)
-		 for i = (read s nil end-of-file)
-		 while (not (eql i end-of-file))
-		 collect i))
-	  do (setf last-call (eval cmd))
-	  finally (return last-call))
-       (get-output-stream-string *standard-output*)
-       (get-output-stream-string *error-output*)))))
+  (let ((sexp (read-whole-string-or-nothing str)))
+    (when sexp
+      (loop 
+	 with *standard-output* = (make-string-output-stream)
+	 with *error-output*    = (make-string-output-stream)
+	 for i in sexp
+	 for ret = (with-retry () (eval i))
+	 finally (return (values ret
+				 (get-output-stream-string *standard-output*)
+				 (get-output-stream-string *error-output*)))))))
+
+(defclass qix-stream (trivial-gray-streams:fundamental-character-input-stream
+		      trivial-gray-streams:trivial-gray-stream-mixin) ())
+
+(define-condition  out-of-data (error)  ())
+
+(defmethod stream-read-char ((this qix-stream))
+  (restart-case (error 'out-of-data)
+    (return-a-char (x) (lambda (x) x))))
 
 
-(handler-bind 
-    ((warning (lambda (x) (print x) nil)))
-  (eval '(progn (warn tmp) (print "helo"))))
+;; (with-retry () 
+;;   (let ((*standard-output* (make-string-output-stream))
+;; 	(*error-output* (make-string-output-stream)))
+;;     (list (eval (read))
+;; 	  (get-output-stream-string *standard-output*)
+;; 	  (get-output-stream-string *debug-io*))))
+
+
+;; (with-retry ()
+;;   (loop 
+;;      with last-call 
+;;      for cmd in  
+;;        (with-input-from-string (s "(print \"hello\") nil")
+;; 	 (loop 
+;; 	    with end-of-file = (gensym)
+;; 	    for i = (read s nil end-of-file)
+;; 	    while (not (eql i end-of-file))
+;; 	    collect i))
+;;      do (setf last-call (eval cmd))
+;;      finally (return last-call)))
+
+
+
+;; (defun eval-string (str)
+;;   (with-retry ()
+;;     (let ((*standard-output* (make-string-output-stream))
+;; 	  (*error-output* (make-string-output-stream)))
+;;       (list 
+;;        (loop 
+;; 	  with last-call 
+;; 	  for cmd in  
+;; 	    (with-input-from-string (s str)
+;; 	      (loop 
+;; 		 with end-of-file = (gensym)
+;; 		 for i = (read s nil end-of-file)
+;; 		 while (not (eql i end-of-file))
+;; 		 collect i))
+;; 	  do (setf last-call (eval cmd))
+;; 	  finally (return last-call))
+;;        (get-output-stream-string *standard-output*)
+;;        (get-output-stream-string *error-output*)))))
+
+
+;; (handler-bind 
+;;     ((warning (lambda (x) (print x) nil)))
+;;   (eval '(progn (warn tmp) (print "helo"))))
